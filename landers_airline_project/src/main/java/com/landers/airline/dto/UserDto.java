aa75e7098bd98186e6aa3d80d512bdddbf2d9e33
hakiseong <ks510222@naver.com>
2024-02-06 11:22:21 +0900

    수정
    안되나요

--- landers_airline_project/src/main/java/com/landers/airline/dto/UserDto.java
+++ landers_airline_project/src/main/java/com/landers/airline/dto/UserDto.java
@@ -11,7 +11,7 @@ public class UserDto {
 	public UserDto() {
 		
 	}
-
+	
 	public UserDto(String user_id, String user_pw, String user_name, String user_phone, String user_address) {
 		super();
 		this.user_id = user_id;
