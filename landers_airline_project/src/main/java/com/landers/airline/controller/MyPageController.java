ca38ed090183df32858686bf1b68a8bbb41d539d
shinminchan <althwkd18@naver.com>
2024-02-08 17:49:52 +0900

    설날전 종합.
    지금 마이페이지 안됨.

--- landers_airline_project/src/main/java/com/landers/airline/controller/MyPageController.java
+++ landers_airline_project/src/main/java/com/landers/airline/controller/MyPageController.java
@@ -1,12 +1,14 @@
 package com.landers.airline.controller;
 
 import java.util.Date;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.landers.airline.dto.UserDto;
 import com.landers.airline.service.MyPageService;
 
 @Controller
@@ -19,8 +21,21 @@ public class MyPageController {
 	@GetMapping("mypageHome.do")
 	public String home(Model model) {
 		System.out.println("MyPageController home() " + new Date());
+
+		
 		model.addAttribute("main", "mypageHome");
 		
 		return "mypage/main";
+	}
+	
+	@GetMapping("mylist.do")
+	public String mylist( UserDto dto, HttpServletRequest request) {
+		System.out.println("MyPageController mylist() " + new Date());
+		System.out.println(dto.toString());
+		UserDto mylist = service.mylist(dto);
+		
+		request.setAttribute("mypage", mylist);
+		
+		return "mypage/mypageHome";
 	}
 }
--- landers_airline_project/src/main/java/com/landers/airline/service/impl/MyPageServiceImpl.java
+++ landers_airline_project/src/main/java/com/landers/airline/service/impl/MyPageServiceImpl.java
@@ -1,9 +1,12 @@
 package com.landers.airline.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.landers.airline.dao.MyPageDao;
+import com.landers.airline.dto.UserDto;
 import com.landers.airline.service.MyPageService;
 
 
@@ -12,4 +15,24 @@ public class MyPageServiceImpl implements MyPageService {
 
 	@Autowired
 	MyPageDao dao;
+
+	@Override
+	public boolean userUpdate(String user_id) {
+		
+		int count = dao.userUpdate(user_id);
+		return count>0?true:false;
+	}
+
+	@Override
+	public boolean userDelete(String user_id) {
+		
+		int count = dao.userDelete(user_id);
+		
+		return count>0?true:false;
+	}
+
+	@Override
+	public UserDto mylist(UserDto dto) {
+		return dao.mylist(dto);
+	}
 }
