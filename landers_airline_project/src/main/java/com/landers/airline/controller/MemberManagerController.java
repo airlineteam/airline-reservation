1b9bf39df0a5b19df7f5f6bfa26e799dd1e5883e
o220s <pjs47792332@gmail.com>
2024-02-08 09:20:38 +0900

    탈퇴기능추가
    탈퇴기능추가

--- landers_airline_project/src/main/java/com/landers/airline/controller/MemberManagerController.java
+++ landers_airline_project/src/main/java/com/landers/airline/controller/MemberManagerController.java
@@ -6,10 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-
 
 import com.landers.airline.dto.UserDto;
 import com.landers.airline.service.MemberManagerService;
