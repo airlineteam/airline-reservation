44cc4a36d1ab5e28d7652b1bf39823011a0b3669
wonbi Jeong <totoyo693@naver.com>
2024-02-12 18:35:30 +0900

    2/12 calendar 수정
    제발 화면에 뿌려졌으면 좋겠다ㅎ

--- landers_airline_project/src/main/java/com/landers/airline/dao/impl/MytripDaoImpl.java
+++ landers_airline_project/src/main/java/com/landers/airline/dao/impl/MytripDaoImpl.java
@@ -2,12 +2,14 @@ package com.landers.airline.dao.impl;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.landers.airline.dao.MytripDao;
 import com.landers.airline.dto.CalendarDto;
+import com.mysql.cj.Session;
 
 @Repository
 public class MytripDaoImpl implements MytripDao{
@@ -20,4 +22,38 @@ public class MytripDaoImpl implements MytripDao{
 		return session.selectList("mytrip.calendarlist", dto);
 	}
 
+	@Override
+	public String pp(CalendarDto dto) {
+		return session.selectOne("pp", dto);
+	}
+
+	@Override
+	public String p(CalendarDto dto) {
+		return session.selectOne("p", dto);
+	}
+
+	@Override
+	public String nn(CalendarDto dto) {
+		return session.selectOne("nn", dto);
+	}
+
+	@Override
+	public String n(CalendarDto dto) {
+		return session.selectOne("n", dto);
+	}
+
+	@Override
+	public int year(CalendarDto dto) {
+		return session.insert("year", dto);
+	}
+
+	@Override
+	public int month(CalendarDto dto) {
+		return session.insert("month", dto);
+	}
+
+	@Override
+	public int dayOfWeek(CalendarDto dto) {
+		return session.insert("dayOfWeek", dto);
+	}
 }
